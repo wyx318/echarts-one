6ab48d8908e12c688713a27917f9407c4c129396
wyx318 <651033260@qq.com>
2020-04-10 16:22:14 +0800

    完成全部 布局  开始引入Echarts

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//一共七个图表 柱状图 模块一
+(function() {
+    // 1.实例化对象
+    var myChart = echarts.init(document.querySelector('.bar .chart'))
+        //2.指定配置项和数据
+    var option = {
+        color: ['#3398DB'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [{
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }],
+        yAxis: [{
+            type: 'value'
+        }],
+        series: [{
+            name: '直接访问',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+        }]
+    };
+    //把配置项给实例对象
+    myChart.setOption(option)
+})();
